158839291020512086a855a02ad1681f61537a4c
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-05-22 15:48:40 +0530

    Update BotGame.py

--- BotGame.py
+++ BotGame.py
@@ -5,45 +5,52 @@ from numpy import *
 
 class Cordinates():
     replayBtn = (1150,400)
-    dinasaur = (910,550)  #  x=1230 and y= 560 when dino was down so 560-410=150
+    dinasaur = (910,560)  #  x=1230 and y= 560 when dino was down so 560-410=150
     #580=x coordinates to check for tree (dino should jump when obstacle appears)
     #y coordinates = 710 it is for the lowest obstacle
     
     
 def restartGame():
     pyautogui.click(Cordinates.replayBtn)
-    pyautogui.keyDown('down')
-    
+    #pyautogui.keyDown('down')
+     
+restartGame()
+
 def pressSpace():
-    pyautogui.keyUp('down')
-    pyautogui.keyDown('space')
-    print("Jump")
-    sleep(0.18)
-    pyautogui.keyUp('space')
-    pyautogui.keyDown('down')
+    #pyautogui.keyUp('down')
+  pyautogui.press('space') 
+    #     pyautogui.keyDown('down')
     
+'''
+restartGame()
+sleep(1) 
+pressSpac e()
+
+'''
 def imageGrab():
-    box=(950,410,1040,430)   #box is the area where any obstacle is detected
+    box=(558,305,696,328)   #box is the area where any obstacle is detected
     image= ImageGrab.grab(box)
     grayImage=ImageOps.grayscale(image)
-    '''
+    '''    
     analysing images to  grayscale is much more efficient than in color so we 
     convert the grabbed image to grayscale
-    '''
+    '''    
     a=array(grayImage.getcolors())
     #now we print out the grayedscaled sum of all the colour values of each of pixel in the box
     print(a.sum())
     return a.sum()
-    
+
+  
 def main():
     restartGame()
     while True:
-        if(imageGrab()!=2047):
+        if(imageGrab()!=3421):
             pressSpace()
             sleep(0.1)
         
 main()
 
+    
     
         
     
